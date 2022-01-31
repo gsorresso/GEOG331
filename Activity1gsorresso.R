c33207e0e23ae1591f8781e3e6c4386a98cb15a4
gsorresso <gsorresso@colgate.edu>
2022-01-31 11:49:26 -0500

    Added vector math practice

--- Activity1gsorresso.R
+++ Activity1gsorresso.R
@@ -1,3 +1,19 @@
-##gsorresso script 
+##gsorresso script Activity 1 
+
+##Question 1
 print("Hello World!")
+
+##Question 2
+
+##practice math
+100+80
+##create numerical vectors
+vector1 <- c(30, 60, 90, 120) 
+vector2 <- c(10, 20, 30, 40)
+vector3 <- c(20, 40, 60, 80) 
+
+##math with new vectors 
+sum(vector1, vector2)
+vector3/2
+vector2/vector1
 
