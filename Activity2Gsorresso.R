735539a17a77af899f68c93e3ef32c459dc08f58
gsorresso <gsorresso@colgate.edu>
2022-02-11 12:11:05 -0500

    almost finished act 2

--- Activity2Gsorresso.R
+++ Activity2Gsorresso.R
@@ -35,6 +35,7 @@ Mat.bycol[1,2]
 Mat.bycol[1,]
 ##look at all values in column 2  
 Mat.bycol[,2]
+
 ###############################QUESTION 2#######################################
 ##character vector 
 char_vector <- c("colgate", "has", "no", "drinking", "water")
@@ -46,11 +47,12 @@ int_vector <- c(1L, 2L, 3L, 4L, 5L)
 fac_vector <- factor(c("no drinking water", "drinking water", "no drinking water"))
 
 unclass(fac_vector)
+
 ################################DATA FRAMES####################################
 ##Read in Data
 datW <- read.csv("datafolder/2011124.csv", stringsAsFactors = T)
 
-##set first row
+##see first row
 datW[1,]
 
 ##get more infor about the metadata
@@ -73,9 +75,10 @@ mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
 mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm=TRUE)
 #calculate avg daily temp  (halfway between min and max temp)
 datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+##length function 
 
 #get mean across all sites 
-averageTemp <- aggregate(datW$TAVE, by=list(datW$Name), FUN="mean", na.rm=TRUE)
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean", na.rm=TRUE)
 averageTemp
 
 ##change auto output to be more meaningful 
@@ -86,8 +89,8 @@ averageTemp
 datW$siteN <- as.numeric(datW$NAME)
 ##add code to run all four histograms together 
 par(mfrow=c(2,2))
-##make a histogram for the first site in our levels
-hist(datW$TAVE[datW$siteN == 1],
+##make a histogram for the first site in our levels and save it as h1 
+h1<- hist(datW$TAVE[datW$siteN == 1],
      freq = FALSE,
      main = paste(levels(datW$NAME)[1]),
      xlab = "Average daily Temp (degrees C)",
@@ -123,12 +126,12 @@ abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm=TRUE),
        col = "tomato3",
        lwd = 3)
 # add sd line below mean with red color and thickness 3 
-abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE) - sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE) - sd(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
        col = "tomato3",
        lty = 3,
        lwd = 3)
 # add sd line above mean with red color and thickness 3 
-abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE) + sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+abline(v = mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE) + sd(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
        col = "tomato3",
        lty = 3,
        lwd = 3)
@@ -145,12 +148,12 @@ abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm=TRUE),
        col = "tomato3",
        lwd = 3)
 # add sd line below mean with red color and thickness 3 
-abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm = TRUE) - sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm = TRUE) - sd(datW$TAVE[datW$siteN == 4], na.rm = TRUE),
        col = "tomato3",
        lty = 3,
        lwd = 3)
 # add sd line above mean with red color and thickness 3 
-abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm = TRUE) + sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+abline(v = mean(datW$TAVE[datW$siteN == 4], na.rm = TRUE) + sd(datW$TAVE[datW$siteN == 4], na.rm = TRUE),
        col = "tomato3",
        lty = 3,
        lwd = 3)
@@ -167,14 +170,96 @@ abline(v = mean(datW$TAVE[datW$siteN == 5], na.rm=TRUE),
        col = "tomato3",
        lwd = 3)
 # add sd line below mean with red color and thickness 3 
-abline(v = mean(datW$TAVE[datW$siteN == 5], na.rm = TRUE) - sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+abline(v = mean(datW$TAVE[datW$siteN == 5], na.rm = TRUE) - sd(datW$TAVE[datW$siteN == 5], na.rm = TRUE),
        col = "tomato3",
        lty = 3,
        lwd = 3)
 # add sd line above mean with red color and thickness 3 
-abline(v = mean(datW$TAVE[datW$siteN == 5], na.rm = TRUE) + sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+abline(v = mean(datW$TAVE[datW$siteN == 5], na.rm = TRUE) + sd(datW$TAVE[datW$siteN == 5], na.rm = TRUE),
        col = "tomato3",
        lty = 3,
        lwd = 3)
 
 ##probability distributions 
+#seq function generate a seq of numbers that we can use to plot the normal across the range of temperature values 
+x.plot <- dnorm(seq(-10,30, length.out = 100),
+      mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+      sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE)) 
+
+#the dnorm function will produce a variable the probability density based on a mean and standard dev. 
+y.plot <- dnorm(seq(-10,30, length.out = 100),
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE)) 
+                
+##create a density that is scaled to fit the plot since the density has a different range from the data density 
+##helpful for putting things on the same plot to make both plots share a max 
+y.scaled <- (max(h1$density)/max(y.plot))* y.plot 
+                
+## points function adds points of lines to a graph, the first two arguments are the x coordinates and y coordinators 
+points(x.plot, 
+    y.scaled, 
+    type = "l", 
+    col = "royalblue3", 
+    lwd = 4,
+    lty = 2)
+               
+## run documentation on dnorm to get more info on probability distributions 
+help(dnorm) 
+                
+#probability of freezing temps 
+# syntax: pnorm(value to evaluate at(& below), mean, sd) 
+pnorm (0, 
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE)) 
+                
+## probability of temps under 5 degrees 
+pnorm (5, 
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE)) 
+                
+##find probability of temps between 0 - 5 degrees 
+pnorm (5, 
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE))  - pnorm (0,
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE)) 
+                
+##probability of a temperature above 20 degrees C 
+## know that pnorm gives all values under 20 so we can subtract this from 1 
+1 - pnorm (20, 
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE))
+                
+## use qnorm to find all high temp values that happen with less than 5% probability 
+qnorm (0.95, 
+    mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE), 
+    sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE))
+                
+##Q7 Histogram for Daily Precipitation in Aberdeen 
+hist(datW$PRCP[datW$siteN == 1],
+    freq = FALSE,
+    main = paste(levels(datW$NAME)[1]),
+    xlab = "Daily Percipitation (cm) ",
+    ylab = "Relative Frequency",
+    col = "azure3",
+    border = "white")
+
+##Q8 Get the precipitation for each year and site and then make a histogram for one site of annual precipitation
+AnnualP <- aggregate(datW$PRCP, by=list(datW$NAME, datW$year), FUN="sum", na.rm=TRUE)
+AnnualP
+colnames(AnnualP) <- c("NAME", "YEAR", "Percip")
+
+##Q7 Histogram for Annual Percip 
+hist(AnnualP$Percip[AnnualP$NAME == "LIVERMORE, CA US"],
+     freq = FALSE,
+     main = "Annual Percipitation in Livermore CA",
+     xlab = "Daily Percipitation (cm) ",
+     ylab = "Relative Frequency",
+     col = "azure3",
+     border = "white")
+
+##Q9 Mean annual Precipitation for all sites 
+MeanAnnualPercip <- aggregate(AnnualP, by=list(AnnualP$NAME), FUN ="mean", na.rm = TRUE)
+MeanAnnualPercip
+
+                    
