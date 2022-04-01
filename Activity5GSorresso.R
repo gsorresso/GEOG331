6488355efc8cf4ec89cf20351b9128125fb05114
gsorresso <gsorresso@colgate.edu>
2022-04-01 11:48:45 -0400

    act 5 work

--- Activity5GSorresso.R
+++ Activity5GSorresso.R
@@ -5,7 +5,7 @@ setwd("Z://students/gsorresso/")
 
 #load in lubridate
 library(lubridate)
-
+library(ggplot2)
 #read in stream flow data 
 datH <- read.csv("datafolder/stream_flow_data.csv", na.strings=c("Eqp"))
 #look at headers of datH 
@@ -52,7 +52,10 @@ datP$hour <- hour(dateP ) + (minute(dateP )/60)
 datP$decDay <- datP$doy + (datP$hour/24)
 #calculate a decimal year, but account for leap year
 datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/366),
-                       datP$year + (datP$decDay/365))   
+                       datP$year + (datP$decDay/365))  
+#q2 results of leap year function 
+leap_year(datP$year)
+
 #plot discharge 
 plot(datD$decYear, datD$discharge, type = "l", xlab ="Year", ylab = expression(paste("Discharge ft"^"3 ", "sec"^"-1")))
 
@@ -147,10 +150,8 @@ legend("topright", c("mean","1 standard deviation", "2017 observations"), #legen
 ##QUESTION 7 
 #create a data frame that indicates what days have a full 24 hours of precipitation measurements 
 ##plot all discharge measures and symbolize days that have precipitation measures available 
-
-
-
-
+full_day <- aggregate(datP, by=list(datP$doy, datP$year), FUN = length)
+list(full_day)
 
 
 
@@ -197,7 +198,41 @@ for(i in 1:nrow(hydroP)){
 #################################################################################
 ##QUESTION 8
 #make another hydro graph during the winter 
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 302 & datD$doy < 304 & datD$year == 2008,]
+hydroP <- datP[datP$doy >= 302 & datP$doy < 304 & datP$year == 2008,]
+##look at minimum hydro flow during period 
+min(hydroD$discharge)
 
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#ceiling rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+#expand margins 
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+  polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+            hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+          c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+          col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
 
 
 #################################################################################
@@ -238,18 +273,19 @@ ggplot(data= datD16, aes(season,discharge)) +
   ggtitle("Discharge in 2016 by Season") 
 
 #subset 2017 data 
+datD17 <- datD[datD$year == 2017,]
 
 #create a season categorical variable 
 datD17$seasonC <- 0 
-datD17$seasonC <- ifelse(datD16$doy >= 60 & datD16$doy <= 151, 1, datD16$season)
-datD17$seasonC <- ifelse(datD16$doy >= 152 & datD16$doy <= 243, 2, datD16$season)
-datD17$seasonC <- ifelse(datD16$doy >= 244 & datD16$doy <= 302, 3, datD16$season)
+datD17$seasonC <- ifelse(datD17$doy >= 60 & datD17$doy <= 151, 1, datD17$season)
+datD17$seasonC <- ifelse(datD17$doy >= 152 & datD17$doy <= 243, 2, datD17$season)
+datD17$seasonC <- ifelse(datD17$doy >= 244 & datD17$doy <= 302, 3, datD17$season)
 
 #create as factor 
-datD16$season <- cut(datD16$seasonC, 4, labels = c('Winter', 'Spring', 'Summer', "Fall"))
+datD17$season <- cut(datD17$seasonC, 4, labels = c('Winter', 'Spring', 'Summer', "Fall"))
 
 #make a violin plot
-ggplot(data= datD16, aes(season,discharge)) + 
+ggplot(data= datD17, aes(season,discharge)) + 
   geom_violin() + 
   ggtitle("Discharge in 2017 by Season")
 
