4b5dc3d4cc08d428164e2cb713269aaa06d896af
gsorresso <gsorresso@colgate.edu>
2022-04-13 11:59:52 -0400

    work on spacial data

--- ParticipationAct46.R
+++ ParticipationAct46.R
@@ -16,4 +16,32 @@ plotRGB(p, r = 3, g = 2, b = 1,
         scale = 65535,
         stretch = "hist") 
 
+#read file with field observations of canopy cover 
+tree <- read.csv("Z:/data/rs_data/siberia_stand_data.csv",
+                 header = T)
+
+#convert to vector object using terra package 
+gtree <- vect(tree, geom = c("Long", "Lat"), "epsg:4326")
+
+#min and max long and lat
+ext(gtree)
+ext(p)
+
+#project the data to match the cordinate system of the raster layer 
+gtree2 <- project(gtree,p)
+ext(gtree2)
+
+#create a polygon from the extent of the points 
+b <- as.lines(ext(gtree), "epsg:4326")
+
+#reproject the polygons to the same projection as our raster
+b2 <- project(b,crs(p))
+
+#buffer the extent by 200m 
+b3 <- buffer(b2, width = 200)
+
+#use 
+
+
+
 
